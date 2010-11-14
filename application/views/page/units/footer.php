e4da45c1d92b6a6b53a3363fc0bbba916dab0b46
Richard <willis.rh@gmail.com>
2010-11-14 19:54:24 +0000

    updated footer

--- application/views/page/units/footer.php
+++ application/views/page/units/footer.php
@@ -1,9 +1,8 @@
 <footer>
-	<?php if (Kohana::$environment === Kohana::DEVELOPMENT and $_SERVER['HTTP_HOST'] !== 'm.dev.2do.me.uk'){?>
-		<div id="application-profiler" style="display: none">
-			{profiler}
-			{execution_time}
-		</div>
-	<?php }?>
+<?php if (Kohana::$environment === Kohana::DEVELOPMENT){?>
+	<div id="profiler">
+		{execution_time}
+	</div>
+<?php }?>
 </footer>
 
