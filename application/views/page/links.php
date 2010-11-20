d86f489a129478dad468cd4c67f6dc2fb123bff2
Richard Willis <willis.rh@gmail.com>
2010-11-20 20:19:26 +0000

    added links page view

---
+++ application/views/page/links.php
@@ -0,0 +1,5 @@
+<ul>
+	<li><a href="http://kohanaframework.org/guide/about.kohana">Official docs</a></li>
+	<li><a href="http://kohanaframework.org/guide/api">API browser</a></li>
+	<li><a href="http://kerkness.ca/wiki/doku.php">Unofficial docs</a></li>
+</ul>
