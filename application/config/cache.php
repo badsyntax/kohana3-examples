54c948552a979b2d3a1872a0442c1cfd91fda573
Richard Willis <willis.rh@gmail.com>
2010-11-20 18:13:02 +0000

    added cache config

---
+++ application/config/cache.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+	'file'	  => array
+	(
+		'driver'		=> 'file',
+		'cache_dir'		=> 'application/cache/',
+		'default_expire'	=> 3600,
+	)
+);
