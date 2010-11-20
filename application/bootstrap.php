121cdfae6a843cc23901b891ecf1b0df9eb8dc82
Richard Willis <willis.rh@gmail.com>
2010-11-20 00:30:07 +0000

    enabled swiftmailer module

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -77,13 +77,14 @@ Kohana::$config->attach(new Kohana_Config_File);
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 Kohana::modules(array(
-	'auth'       => MODPATH.'auth',       // Basic authentication
+	'auth'		=> MODPATH.'auth',       // Basic authentication
+	'database'	=> MODPATH.'database',   // Database access
+	'orm'		=> MODPATH.'orm',        // Object Relationship Mapping
+	'oauth'		=> MODPATH.'oauth',      // OAuth authentication
+	'swiftmailer'	=> MODPATH.'swiftmailer'
 	// 'cache'      => MODPATH.'cache',      // Caching with multiple backends
 	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
-	'database'   => MODPATH.'database',   // Database access
 	// 'image'      => MODPATH.'image',      // Image manipulation
-	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
-	'oauth'      => MODPATH.'oauth',      // OAuth authentication
 	// 'pagination' => MODPATH.'pagination', // Paging of results
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
