d69ce3a33b1f222a8f5195d7952863e2fe1b6514
Richard Willis <willis.rh@gmail.com>
2010-11-20 20:18:50 +0000

    added auth messages

---
+++ application/messages/signup.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+ 
+return array(
+    'username.username_available' => 'username has been taken',
+    'email.email_available' => 'a profile is already using the email',
+);
