31f537cd7a1a1348fe2d1700cfd1d37312e3a783
Richard Willis <willis.rh@gmail.com>
2010-11-21 12:57:45 +0000

    removed request caching

--- application/classes/controller/base.php
+++ application/classes/controller/base.php
@@ -8,16 +8,10 @@ class Controller_Base extends Controller_Template {
 
 	public $is_mobile = FALSE;
 
-	public $cache_request = FALSE;
-
-	public $cache_key = FALSE;
-  
 	public function before()
 	{
 		$this->authenticate();
 		
-		$this->cache_request AND $this->cache();
-
 		if ($this->is_mobile = strstr($_SERVER['HTTP_HOST'], 'm.')) $this->template .= '_mobile';
 		
 		parent::before();
@@ -31,18 +25,9 @@ class Controller_Base extends Controller_Template {
 	{
 		parent::after();
 
-		$this->cache_key !== FALSE AND $this->cache_save();
-			
 		$this->request->response = $this->profile( $this->request->response );
 	}
 
-	public function action_purge_cache()
-	{
-		Cache::instance()->delete_all();
-
-		$this->request->redirect('');
-	}
-		
 	private function authenticate()
 	{
 		if ( $this->auth_required !== FALSE AND Auth::instance()->logged_in($this->auth_required) === FALSE) {
@@ -51,30 +36,6 @@ class Controller_Base extends Controller_Template {
 		}
 	}
 
-	private function cache()
-	{
-		$this->cache_key = $this->request_hash();
-
-		$cache = Cache::instance()->get($this->cache_key);
-
-		if ( $cache ) {
-
-			$this->request->send_headers();
-
-			exit( $this->profile($cache, TRUE) );
-		}
-	}
-
-	private function cache_save($cache_lifetime=FALSE){
-
-		if ($cache_lifetime === FALSE) {
-
-			$cache_lifetime = PHP_INT_MAX;
-		}
-
-		Cache::instance()->set($this->cache_key, (string) $this->request->response, $cache_lifetime);
-	}
-	
 	private function profile($response, $cached=FALSE)
 	{
 		$profiler = Profiler::application();
@@ -89,15 +50,5 @@ class Controller_Base extends Controller_Template {
 		);
 
 		return strtr( (string) $response, $data);
-	}
-
-	private function request_hash()
-	{
-		return sha1(
-			Auth::instance()->get_user() .
-			$this->request->uri .
-			get_class($this) .
-			implode('', $_REQUEST)
-		);
 	}
 }
