f9a651e41cbfb76be4ee4fd29ec791a6615b92b7
Richard Willis <willis.rh@gmail.com>
2010-11-21 12:56:50 +0000

    added error controller

---
+++ application/classes/controller/error.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Error extends Controller_Base {
+
+	public function action_index($type="404")
+	{
+		$this->template->title = "{$type}";
+		$this->template->content = new View("page/error_{$type}");
+	}
+}
