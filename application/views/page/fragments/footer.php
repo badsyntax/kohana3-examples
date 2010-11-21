bb25ac4ed70a424e9c07e94fc5eb738e3293fa91
Richard Willis <willis.rh@gmail.com>
2010-11-21 22:20:24 +0000

    simplified footer

--- application/views/page/fragments/footer.php
+++ application/views/page/fragments/footer.php
@@ -1,11 +1,8 @@
 <footer>
 	<?php if (Kohana::$environment === Kohana::DEVELOPMENT){?>
-		<div id="profiler">
-			<div class="helper-right">
-				Powered by <a href="http://kohanaframework.org/">KO3</a>
-			</div>
+		<!--
 			{execution_time} - {memory_usage} {cached}
-		</div>
+		-->
 	<?php }?>
 </footer>
 
