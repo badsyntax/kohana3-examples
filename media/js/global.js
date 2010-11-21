f90d765bdc89aada2e312cb8832d393da3770d09
Richard Willis <willis.rh@gmail.com>
2010-11-21 13:00:26 +0000

    added html5 shim to global js

--- media/js/global.js
+++ media/js/global.js
@@ -1,4 +1,7 @@
 (function($, document){
+
+	/* html5-shim by @kangax */
+	/*@cc_on(function(e,i){i=e.length;while(i--)document.createElement(e[i])})("abbr,article,aside,audio,canvas,details,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','))@*/
 	
 	document.documentElement.className += ' js';
 
