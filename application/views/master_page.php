e8defd700e97cc7ed9e4f920cea1f7ddefa8dd30
Richard Willis <willis.rh@gmail.com>
2010-11-23 18:15:03 +0000

    added environment class name to html tag so JS can grab this value

--- application/views/master_page.php
+++ application/views/master_page.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en" class="no-js" dir="ltr">
+<html lang="en" class="no-js <?php echo Kohana::$environment?>" dir="ltr">
 <head>
 	<meta charset="utf-8" />
       	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
