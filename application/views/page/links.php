237939e7c7c38e26028257798173c2432508b930
Richard Willis <willis.rh@gmail.com>
2010-11-23 21:00:29 +0000

    removed links page

--- application/views/page/links.php
+++
@@ -1,6 +0,0 @@
-<h1>Kohana 3 links</h1>
-<ul>
-	<li><a href="http://kohanaframework.org/guide/about.kohana">Official docs</a></li>
-	<li><a href="http://kohanaframework.org/guide/api">API browser</a></li>
-	<li><a href="http://kerkness.ca/wiki/doku.php">Unofficial docs</a></li>
-</ul>
