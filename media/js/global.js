375d3387ece3812da09ddeb93974e885406fcaea
Richard Willis <willis.rh@gmail.com>
2010-11-23 18:12:31 +0000

    added setvars and setjs functions

--- media/js/global.js
+++ media/js/global.js
@@ -1,12 +1,28 @@
 (function($, document){
 
-	/* html5-shim by @kangax */
+	// html5-shim by @kangax
 	/*@cc_on(function(e,i){i=e.length;while(i--)document.createElement(e[i])})("abbr,article,aside,audio,canvas,details,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','))@*/
-	
-	document.documentElement.className += ' js';
 
-	// document ready
-	$(function(){
+	// add 'js' classname to <html> tag
+	function setJs(){
+
+		var classes = document.documentElement.className.split(' ');
+
+		classes.push('js');
+
+		document.documentElement.className = classes.join(' ');
+	}
+
+	// set global vars
+	function setVars(){
+
+		window._environment = /\bproduction\b/.test( document.documentElement.className ) 
+			? 'production' 
+			: 'development';
+	}
+
+	// bind profiler click event handler
+	function profiler(){
 		
 		var 
 			profiler = $('#profiler-container'), 
@@ -14,11 +30,21 @@
 
 		profiler.length && anchor.length && anchor
 			.click(function(){
-
+	
+				// show/hide the profiler
 				profiler.fadeToggle('fast');
 
+				// toggle the show/hide icon
 				anchor.find('span').toggle();
 			});
+	}
+
+	// document ready
+	$(function(){
+
+		setJs();
+		setVars();
+		profiler();
 	});
 
 })(this.jQuery, document);
