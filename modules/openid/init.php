fe71e335d55ba91d34d45ad0d69e7302586c23fe
Richard Willis <willis.rh@gmail.com>
2010-11-23 18:17:37 +0000

    added openid module init

---
+++ modules/openid/init.php
@@ -0,0 +1,10 @@
+<?php  
+        ini_set('include_path', 'modules/openid/vendor');
+
+	// start the session
+	Session::instance();
+
+        require_once Kohana::find_file('vendor', 'Auth/OpenID/Consumer');
+        require_once Kohana::find_file('vendor', 'Auth/OpenID/FileStore');
+        require_once Kohana::find_file('vendor', 'Auth/OpenID/SReg');
+        require_once Kohana::find_file('vendor', 'Auth/OpenID/PAPE');
