5e9c853c962ee02a4ca84eee8a3e11abc88d0b1e
Richard Willis <willis.rh@gmail.com>
2010-11-23 15:35:29 +0000

    added cookie class extension to set adjust the cookie configuration

---
+++ application/classes/cookie.php
@@ -0,0 +1,38 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * see http://kohanaframework.org/guide/using.sessions#cookie-settings
+ */
+
+class Cookie extends Kohana_Cookie {
+
+        /**
+         * @var  string  Magic salt to add to the cookie
+         */
+        public static $salt = 'kooky';
+
+        /**
+         * @var  integer  Number of seconds before the cookie expires
+         */
+        public static $expiration = 0;
+
+        /**
+         * @var  string  Restrict the path that the cookie is available to
+         */
+        public static $path = '/';
+
+        /**
+         * @var  string  Restrict the domain that the cookie is available to
+         */
+        public static $domain = NULL;
+
+        /**
+         * @var  boolean  Only transmit cookies over secure connections
+         */
+        public static $secure = FALSE;
+
+        /**
+         * @var  boolean  Only transmit cookies over HTTP, disabling Javascript access
+         */
+        public static $httponly = FALSE;
+ 
+} // End Cookie
