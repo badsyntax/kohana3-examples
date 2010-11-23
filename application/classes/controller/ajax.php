27ba9a1a00f112eea12eeb6c3cfbebe7f701244e
Richard Willis <willis.rh@gmail.com>
2010-11-23 20:47:34 +0000

    added xml and json examples

--- application/classes/controller/ajax.php
+++ application/classes/controller/ajax.php
@@ -5,17 +5,43 @@ class Controller_Ajax extends Controller_Base {
 	public function action_index()
 	{
 		$this->template->title = 'Kohana3 AJAX Examples';
-		$this->template->content = new View('page/ajax');
+		$this->template->content = View::factory('page/ajax')
+			->bind('errors', $errors);
+
+		$data = Validate::factory($_POST)
+			->filter('email', 'trim')
+			->rule('email', 'not_empty')
+			->rule('email', 'email')
+			->filter('message', 'trim')
+			->filter('message', 'Security::xss_clean')
+			->filter('message', 'strip_tags')
+			->rule('message', 'not_empty');
+		
+		if ($data->check()){
+			
+			$this->template->content->message = 'Email successfully sent!';
+		}
+
+		$_POST = $data->as_array();
+
+		$errors = $data->errors('contact');
+
+		if ( Request::$is_ajax ) {
+		
+			$this->template->content = json_encode($errors);
+		
+			$this->request->headers['Content-Type'] = 'application/json';
+		}
 	}
 
 	public function action_html()
 	{
-		$this->template->content = new View('ajax/html');
+		$this->template->content = View::factory('ajax/html');
 	}
 
 	public function action_xml()
 	{
-		$this->template->content = new View('ajax/xml');
+		$this->template->content = View::factory('ajax/xml');
 
 		$this->request->headers['Content-Type'] = 'text/xml';
 	}
