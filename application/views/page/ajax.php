54b16227c7a5709cf736b4f47f0ff8bb09869e92
Richard Willis <willis.rh@gmail.com>
2010-11-23 21:01:11 +0000

    moving code around

--- application/views/page/ajax.php
+++ application/views/page/ajax.php
@@ -16,11 +16,52 @@
 
 </div>
 
+<br />
+
+<h2>AJAX form validation</h2>
+
+<p>This example demonstrates using back-end form validation, and errors displayed via AJAX. If Javascript is disabled the form is still fully functional.</p>
+
+<?= Form::open(NULL, array('id' => 'ajax-form'))?>
+	<fieldset>
+
+		<legend>Contact</legend>
+
+		<p class="form-success helper-hidden"><?php echo isset($message) ? $message : ''?></p>
+
+		<div class="field">
+			<label for="field-email">
+				Email
+				<span class="form-error">
+				<?php if (isset($errors['email'])){?>
+					<?php echo $errors['email']?>
+				<?php }?>
+				</span>
+			</label>
+			<?php echo Form::input('email', $_POST['email'], array('id' => 'field-email'))?>
+		</div>
+
+		<div class="field">
+			<label for="field-message">
+				Message
+				<span class="form-error">
+				<?php if (isset($errors['message'])){?>
+						<?php echo $errors['message']?>
+				<?php }?>
+				</span>
+			</label>
+			<?php echo Form::textarea('message', $_POST['message'], array('id' => 'field-message', 'style' => 'height: 60px'))?>
+		</div>
+
+		<?php echo Form::submit('submit', 'Submit', array('class' => 'button'))?>
+	</fieldset>
+<?= Form::close()?>
+
+<p>View the source of the page to view the Javascript used for these examples.</p>
+
 <script type="text/javascript">
-
-
 (function($){
-
+	
 	$('#html-link').click(function(e){
 
 		e.preventDefault();
@@ -70,57 +111,9 @@
 		});
 	});
 
-})(this.jQuery);
-
-</script>
-
-<br />
-
-<h2>AJAX form validation</h2>
-
-<p>This example demonstrates using back-end form validation, and errors displayed via AJAX. If Javascript is disabled the form is still fully functional.</p>
-
-<?= Form::open(NULL, array('id' => 'ajax-form'))?>
-	<fieldset>
-
-		<legend>Contact</legend>
-
-		<p class="form-success helper-hidden"><?php echo isset($message) ? $message : ''?></p>
-
-		<div class="field">
-			<label for="field-email">
-				Email
-				<span class="form-error">
-				<?php if (isset($errors['email'])){?>
-					<?php echo $errors['email']?>
-				<?php }?>
-				</span>
-			</label>
-			<?php echo Form::input('email', $_POST['email'], array('id' => 'field-email'))?>
-		</div>
-
-		<div class="field">
-			<label for="field-message">
-				Message
-				<span class="form-error">
-				<?php if (isset($errors['message'])){?>
-						<?php echo $errors['message']?>
-				<?php }?>
-				</span>
-			</label>
-			<?php echo Form::textarea('message', $_POST['message'], array('id' => 'field-message', 'style' => 'height: 60px'))?>
-		</div>
-
-		<?php echo Form::submit('submit', 'Submit', array('class' => 'button'))?>
-	</fieldset>
-<?= Form::close()?>
-
-<script type="text/javascript">
-(function($){
-
 	var form;
 
-	function formSubmitHandler(errors){
+	function postSuccess(errors){
 
 		$('.form-error, .form-success').hide();
 
@@ -159,7 +152,7 @@
 			url: this.action,
 			data: $(this).serialize(),
 			dataType: 'json',
-			success: formSubmitHandler
+			success: postSuccess
 		});
 	});
 
