c2d596498a2a78d4462cdbf9509c339c1da21b88
Richard Willis <willis.rh@gmail.com>
2010-11-24 11:21:07 +0000

    ie6 - adjusted position of right links to float correctly

--- application/views/page/fragments/nav.php
+++ application/views/page/fragments/nav.php
@@ -1,30 +1,29 @@
 <nav>
+	<div class="helper-right">
+	
+		<?php if (Auth::instance()->logged_in()) {?>
+			
+			<a href="<?php echo URL::site('auth/profile')?>">Profile</a>
+			<a href="<?php echo URL::site('auth/signout')?>">Sign out</a>
+
+		<?php } else {?>
+
+			<a href="<?php echo URL::site('auth/signin')?>">Sign in</a>
+			<a href="<?php echo URL::site('auth/signup')?>">Sign up</a>
+		<?php } ?>
+
+		<?php if (Kohana::$environment === Kohana::DEVELOPMENT){?>
+
+			<a href="#profiler">
+				Profiler 
+				<span class="plus">+</span>
+				<span class="minus helper-hidden">&ndash;</span>
+			</a>
+		<?php }?>
+	</div>
+
 	<a href="<?php echo URL::site()?>">Home</a>
 	<a href="<?php echo URL::site('ajax')?>">Ajax</a>
 	<a href="<?php echo URL::site('contact')?>">Contact</a>
 	<a href="<?php echo URL::site('general')?>">General</a>
-
-	<div class="helper-right">
-	
-	<?php if (Auth::instance()->logged_in()) {?>
-		
-		<a href="<?php echo URL::site('auth/profile')?>">Profile</a>
-		<a href="<?php echo URL::site('auth/signout')?>">Sign out</a>
-
-	<?php } else {?>
-
-		<a href="<?php echo URL::site('auth/signin')?>">Sign in</a>
-		<a href="<?php echo URL::site('auth/signup')?>">Sign up</a>
-	<?php } ?>
-
-	<?php if (Kohana::$environment === Kohana::DEVELOPMENT){?>
-
-		<a href="#profiler">
-			Profiler 
-			<span class="plus">+</span>
-			<span class="minus helper-hidden">&ndash;</span>
-		</a>
-	<?php }?>
-
-	</div>
 </nav>
