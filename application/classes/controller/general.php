048e07cd74d0f584143f6f948a8e31b8a557ac47
Richard Willis <willis.rh@gmail.com>
2010-11-26 00:08:15 +0000

    moved ajax controller methods into general controller

--- application/classes/controller/general.php
+++ application/classes/controller/general.php
@@ -21,4 +21,61 @@ class Controller_General extends Controller_Base {
 		$this->template->content = $pagination->render();
 	}
 
+	public function action_ajax()
+	{
+		$this->template->title = __('Kohana3 AJAX Examples');
+		$this->template->content = View::factory('page/ajax')
+			->bind('errors', $errors);
+
+		$data = Validate::factory($_POST)
+			->filter('email', 'trim')
+			->rule('email', 'not_empty')
+			->rule('email', 'email')
+			->filter('message', 'trim')
+			->filter('message', 'Security::xss_clean')
+			->filter('message', 'strip_tags')
+			->rule('message', 'not_empty');
+		
+		if ($data->check()){
+			
+			$this->template->content->message = __('Email successfully sent!');
+		}
+
+		$_POST = $data->as_array();
+
+		$errors = $data->errors('contact');
+
+		if ( Request::$is_ajax ) {
+		
+			$this->template->content = json_encode($errors);
+		
+			$this->request->headers['Content-Type'] = 'application/json';
+		}
+	}
+
+	public function action_ajax_html()
+	{
+		$this->template->content = View::factory('ajax/html');
+	}
+
+	public function action_ajax_xml()
+	{
+		$this->template->content = View::factory('ajax/xml');
+
+		$this->request->headers['Content-Type'] = 'text/xml';
+	}
+
+	public function action_ajax_json()
+	{
+		$data = new stdclass();
+		$data->id = '323';
+		$data->name = 'John';
+		$data->surname = 'Smith';
+		$data->email = 'john@smi.th';
+
+		$this->template->content = json_encode($data);
+		
+		$this->request->headers['Content-Type'] = 'application/json';
+	}
+
 } // End Controller_Generl
