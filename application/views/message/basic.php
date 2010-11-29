39b697d87035357c3aca50776371447d7d5f9497
Richard Willis <willis.rh@gmail.com>
2010-11-29 22:41:00 +0000

    added messages view

---
+++ application/views/message/basic.php
@@ -0,0 +1,9 @@
+<div id="messages">
+	<ul>
+		<?php foreach ($messages as $message) { ?>
+			<li class="<?php echo $message->type ?>">
+				<p><?php echo $message->text ?></p>
+			</li>
+		<?php } ?>
+	</ul>
+</div>
