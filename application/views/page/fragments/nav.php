a4edab2f7c5f2c60f4955f827c6eb04be645ac8f
Richard Willis <willis.rh@gmail.com>
2010-12-02 21:35:32 +0000

    link uri can match full request uri

--- application/views/page/fragments/nav.php
+++ application/views/page/fragments/nav.php
@@ -1,13 +1,12 @@
 <nav>
 	<?php 
 		$nav = Kohana::config('nav');
+		$uri_segments = explode('/', Request::instance()->uri);
 
 		foreach($nav['links'] as $url => $text) {
-
-			$attributes = $url === Request::instance()->uri 
+			$attributes = ($url === Request::instance()->uri OR $url === $uri_segments[0])
 				? array('class' => 'selected') 
 				: NULL;
-
 			echo HTML::anchor($url, $text, $attributes);
 		}
 	?>
