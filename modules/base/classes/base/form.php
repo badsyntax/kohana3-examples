37ed401b6a53cbea2996f98e540b270f414ade77
Richard Willis <willis.rh@gmail.com>
2010-12-02 22:12:28 +0000

    added base module as a submodule

--- modules/base/classes/base/form.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Base_Form extends Kohana_Form { }
--- modules/base/classes/model/user.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-class Model_User extends Model_Base_User { } // End Model_User
