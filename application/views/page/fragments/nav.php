d3e025f4aaa2ed7ce7b5a2e54a87bd441bf25488
Richard Willis <willis.rh@gmail.com>
2010-12-02 20:06:07 +0000

    updated to show selected class on current nav url

--- application/views/page/fragments/nav.php
+++ application/views/page/fragments/nav.php
@@ -1,30 +1,14 @@
 <nav>
-	<div class="helper-right">
-	
-		<?php if (Auth::instance()->logged_in()) {?>
-			
-			<a href="<?php echo URL::site('user/profile')?>">Profile</a>
-			<a href="<?php echo URL::site('user/signout')?>">Sign out</a>
+	<?php 
+		$nav = Kohana::config('nav');
 
-		<?php } else {?>
+		foreach($nav['links'] as $url => $text) {
 
-			<a href="<?php echo URL::site('user/signin')?>">Sign in</a>
-			<a href="<?php echo URL::site('user/signup')?>">Sign up</a>
-		<?php } ?>
+			$attributes = $url === Request::instance()->uri 
+				? array('class' => 'selected') 
+				: NULL;
 
-		<?php if (Kohana::$environment === Kohana::DEVELOPMENT){?>
-
-			<a href="#profiler">
-				Profiler 
-				<span class="plus">+</span>
-				<span class="minus helper-hidden">&ndash;</span>
-			</a>
-		<?php }?>
-	</div>
-
-	<a href="<?php echo URL::site()?>">Home</a>
-	<a href="<?php echo URL::site('contact')?>">Contact</a>
-	<a href="<?php echo URL::site('general')?>">General</a>
-	<a href="<?php echo str_replace('://', 'http://mobile.', URL::base(TRUE, ''))?>">Mobile</a>
-	<a href="<?php echo URL::site('admin')?>">Admin</a>
+			echo HTML::anchor($url, $text, $attributes);
+		}
+	?>
 </nav>
