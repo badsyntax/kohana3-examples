726c6869b66ef87a1a4f8b19c261c1c905d1b87d
Richard Willis <willis.rh@gmail.com>
2010-12-05 12:33:16 +0000

    copy changes

--- application/views/page/contact.php
+++ application/views/page/contact.php
@@ -1,7 +1,6 @@
 <?php if (!Request::$is_mobile){?>
 <p>
-	This simple contact controller demonstrates how to use the Validation class and  
-	SwiftMailer to send email. The contact view demonstrates how to use the Form helper and error handling.
+	This simple contact controller demonstrates how to use the Validation class and SwiftMailer to send email.
 </p>
 <?php }?>
 <?php echo Form::open()?>
@@ -22,8 +21,8 @@
 
 		<div class="field">
 			<?php echo
-				Form::label('field-message', 'Message', NULL, $errors) .
-				Form::textarea('message', $_POST['message'])
+				Form::label('message', 'Message', NULL, $errors) .
+				Form::textarea('message', $_POST['message'], NULL, TRUE, $errors)
 			?>
 		</div>
 
