c6bf4361b6def99ba88c8d245aeba877633bd12d
Richard Willis <willis.rh@gmail.com>
2010-12-31 21:06:56 +0000

    added site config

---
+++ application/config/site.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+/*
+ * General site configuration
+ */
+return array(
+	'contact' => array(
+		'recipient' => array(
+			'example@email.com' => 'Recipient Name'
+		),
+		'subject' => 'Feedback from :name'
+	)
+);
