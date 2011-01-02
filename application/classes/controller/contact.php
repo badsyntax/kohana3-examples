1d6ef6eba7fd9bf16c414c24a66c198e0c088e90
Richard Willis <willis.rh@gmail.com>
2011-01-02 22:04:35 +0000

    email config

--- application/classes/controller/contact.php
+++ application/classes/controller/contact.php
@@ -8,10 +8,6 @@ class Controller_Contact extends Controller_Base {
 		$this->template->content = View::factory('page/contact')
 			->bind('errors', $errors);
 		
-		$recipient = array(
-			'recipient@email.com' => 'Recipient name'
-		);
-
 		// Validate the required fields
 		$data = Validate::factory($_POST)
 			->filter('name', 'trim')
@@ -30,12 +26,16 @@ class Controller_Contact extends Controller_Base {
 			require Kohana::find_file('vendor', 'swiftmailer/lib/swift_required');
 
 			$transport = Swift_MailTransport::newInstance();
-
 			$mailer = Swift_Mailer::newInstance($transport);
+			
+			// Get the email config
+			$config = Kohana::config('site.contact');
+			$recipient = $config['recipient'];
+			$subject = $config['subject'];
 			
 			// Create an email message
 			$message = Swift_Message::newInstance()
-				->setSubject("Feedback from {$data['name']}")
+				->setSubject(__($subject, array(':name' => $data['name'])))
 				->setFrom(array($data['email'] => $data['name']))
 				->setTo($recipient)
 				->addPart($data['message'], 'text/plain');
@@ -43,14 +43,18 @@ class Controller_Contact extends Controller_Base {
 			// Send the message
 			Swift_Mailer::newInstance($transport)->send($message);
 
+			// Set the activity and flash message
+			Activity::set(Activity::SUCCESS, __('Message sent from :email', array(':email' => $data['email'])));
 			Message::set(Message::SUCCESS, __('Message successfully sent.'));
-				
+			
+			// Redirect to prevent POST refresh	
 			$this->request->redirect($this->request->uri);
 		}
 
 		if ($errors = $data->errors('contact'))
 		{
-			 Message::set(Message::ERROR, __('Please correct the errors.'));
+			// Set the error flash message
+			Message::set(Message::ERROR, __('Please correct the errors.'));
 		}
 		
 		$_POST = $data->as_array();
